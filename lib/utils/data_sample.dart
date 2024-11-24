e519a5f5c83d4210574bf4e8d77c77aa26f14173
Muham <muhambriana.31@gmail.com>
2024-11-24 16:58:10 +0700

    update data sample

--- lib/utils/data_sample.dart
+++ lib/utils/data_sample.dart
@@ -3,22 +3,41 @@ import '../utils/config/config.dart';
 
 var courseList = [
   CourseModel(
-    name: 'Belajar Fundamental Aplikasi Flutter',
+    name: 'Belajar Membuat Aplikasi Flutter untuk Pemula',
     pathParent: PathParent.multiPlatform,
     courseLevel: CourseLevel.beginner,
     averageTimeToFinish: 10,
     totalStudents: 999,
     totalModules: 81,
     rating: 4.9,
-    url: 'https://www.dicoding.com/academies/191-memulai-pemrograman-dengan-dart',
     description:
-    'Dart adalah sebuah bahasa pemrograman yang dikembangkan oleh Google dan merupakan bahasa pemrograman resmi untuk Flutter, sebuah UI toolkit dan aplikasi multiplatform dari Google. Flutter sendiri telah digunakan oleh berbagai perusahaan besar seperti Google, Alibaba.com, dan Tencent karena dapat menghemat waktu dan tenaga dengan cara cukup membutuhkan satu codebase untuk mengembangkan aplikasi di berbagai platform, daripada harus menghabiskan waktu untuk membuat codebase terpisah untuk masing-masing platform.',
+    'Sejak dikenalkan oleh Google pada Mei 2017, Flutter langsung menarik perhatian. UI toolkit dari Google ini memungkinkan developer untuk mengembangkan aplikasi di berbagai platform mobile seperti Android dan iOS. Bahkan pada pengembangan terbarunya, Flutter tidak hanya digunakan pada platform mobile, namun sudah merambah ke sisi web dan desktop. Flutter sendiri telah digunakan oleh berbagai perusahaan besar seperti Google, Alibaba.com, dan Tencent.',
     banner: 'https://pikaso.cdnpk.net/private/production/1159864215/render.jpeg?token=exp=1759190400~hmac=5b7e7b77d68408c5a3c84cdb34c930ef7a3f91b788c63cb6760fa6cae5b70f81',
     reviewersPhoto: [
       'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos-d13c84e75ec527250b9ecc6f7079688b20240922021026.png',
       'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos-bd905a077e6dd125b4bcbdc56bb5bd7c20241107133121.png',
       'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos-db5b847be74fdf4ae198d215accdc85b20231226112039.png',
-      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos:7be4a0a8d42738abf94c8033dce75e0020221208201312.png'
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos:7be4a0a8d42738abf94c8033dce75e0020221208201312.png',
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos-feb692cc7d91c66cf026a3521e1230da20240514112204.png',
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos-6b9ebfddee1fdff79d912103c8c9f3ca20240507113604.png',
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos:52ca3572e52e2fce90b6d319ed2a2a3820210929165834.png'
+    ],
+  ),
+  CourseModel(
+    name: 'Belajar Fundamental Aplikasi Flutter',
+    pathParent: PathParent.multiPlatform,
+    courseLevel: CourseLevel.intermediate,
+    averageTimeToFinish: 90,
+    totalStudents: 4850,
+    totalModules: 101,
+    rating: 4.83,
+    description:
+    'Sejak dikenalkan oleh Google pada Mei 2017, Flutter langsung menarik perhatian. UI toolkit dari Google ini memungkinkan developer untuk mengembangkan aplikasi di berbagai platform mobile seperti Android dan iOS. Bahkan pada pengembangan terbarunya, Flutter tidak hanya digunakan pada platform mobile, namun sudah merambah ke sisi web dan desktop. Flutter sendiri telah digunakan oleh berbagai perusahaan besar seperti Google, Alibaba.com, dan Tencent.',
+    banner: 'https://pikaso.cdnpk.net/private/production/1159958634/render.jpeg?token=exp=1759190400~hmac=3bc905b22befc04f299815469ccaad1c4e6c443bcade0f66ec2b7e1d732af399',
+    reviewersPhoto: [
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos-11bd08bac6dc225670139753c20f60bc20240205134622.png',
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos-df64c85f2f70611d293966b01f8c5a9f20240615153103.png',
+      'https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/small/avatar/dos:69dd63f4d64d2b90261e774ceedc217120230122164413.png',
     ],
   ),
 ];
