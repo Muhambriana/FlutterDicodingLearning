c4e34feb2027d79c78dc41b027b7380c3d347aed
Muham <muhambriana.31@gmail.com>
2024-11-27 20:22:01 +0700

    separated page as new class

---
+++ lib/ui/detail/detail_mobile_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import '../../../models/course.dart';
+import '../../../utils/helper.dart';
+import '../../../ui/detail/bookmark_button.dart';
+import '../../../ui/detail/back_button.dart';
+
+
+class DetailMobilePage extends StatelessWidget {
+  final CourseModel course;
+
+  const DetailMobilePage({super.key, required this.course});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Stack(
+                  children: [
+                    _buildBannerAndBackButton(context),
+                    _buildGradientSection(constraints, course),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildBannerAndBackButton(BuildContext context) {
+    return Container(
+      alignment: Alignment.bottomCenter,
+      child: Stack(
+        children: [
+          Image.network(course.banner),
+          const SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  BackButtonDicoding(),
+                  BookmarkButton(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGradientSection(BoxConstraints constraints, CourseModel course) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+          minWidth: constraints.maxWidth, minHeight: constraints.maxHeight),
+      child: IntrinsicHeight(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 400, 0, 0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Color(0xff353f53), Color(0xff212734)]
+                    ),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35.0),
+                      topRight: Radius.circular(35.0),
+                    ),
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _InfoColumn(
+                            icon: Icons.star_rate,
+                            label: course.rating.toString()),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _InfoColumn extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const _InfoColumn({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon),
+        const SizedBox(height: 8.0),
+        Text(label, textAlign: TextAlign.center, style: Utils.getInformationTextStyle()),
+      ],
+    );
+  }
+}
---
+++ lib/ui/detail/detail_web_page.dart
@@ -0,0 +1,175 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import '../../../models/course.dart';
+import '../../../utils/strings.dart';
+import '../../../ui/detail/bookmark_button.dart';
+import '../../utils/helper.dart';
+
+class DetailWebPage extends StatefulWidget {
+  final CourseModel course;
+
+  const DetailWebPage({super.key, required this.course});
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 64,
+        ),
+        child: Center(
+          child: SizedBox(
+            width: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  Strings.appName,
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(widget.course.banner),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          ScrollConfiguration(
+                            behavior: ScrollConfiguration.of(context).copyWith(
+                                dragDevices: {
+                                  PointerDeviceKind.touch,
+                                  PointerDeviceKind.mouse
+                                }),
+                            child: Scrollbar(
+                              controller: _scrollController,
+                              child: Container(
+                                height: 150,
+                                padding: const EdgeInsets.only(bottom: 16),
+                                child: ListView(
+                                  controller: _scrollController,
+                                  scrollDirection: Axis.horizontal,
+                                  children:
+                                  widget.course.reviewersPhoto.map((url) {
+                                    return Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.network(url),
+                                      ),
+                                    );
+                                  }).toList(),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 32,
+                    ),
+                    Expanded(
+                      child: Card(
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Text(
+                                  widget.course.name,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 30.0,
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.calendar_today),
+                                      const SizedBox(width: 8.0),
+                                      Text(
+                                        widget.course.name,
+                                        style: Utils.getInformationTextStyle(),
+                                      )
+                                    ],
+                                  ),
+                                  const BookmarkButton()
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(Icons.access_time),
+                                  const SizedBox(
+                                    width: 8.0,
+                                  ),
+                                  Text(
+                                    widget.course.name,
+                                    style: Utils.getInformationTextStyle(),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(Icons.monetization_on),
+                                  const SizedBox(
+                                    width: 8.0,
+                                  ),
+                                  Text(
+                                    widget.course.name,
+                                    style: Utils.getInformationTextStyle(),
+                                  )
+                                ],
+                              ),
+                              Container(
+                                padding:
+                                const EdgeInsets.symmetric(vertical: 16.0),
+                                child: Text(
+                                  widget.course.description,
+                                  textAlign: TextAlign.justify,
+                                  style: const TextStyle(fontSize: 16.0),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+}
