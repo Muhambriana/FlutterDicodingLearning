6c338f006da35fdbfc5c594729713aa299d5fdb0
Muham <muhambriana.31@gmail.com>
2024-11-30 18:50:02 +0700

    add new app color resources

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Colors.blue;
   static const Color primaryBlue = Color(0xFF4E4AF2);
+  static const Color nonPrimaryText = Colors.white54;
 }
