9a7147fc9a47968caade7e183dccba9a9ae80396
Muham <muhambriana.31@gmail.com>
2024-12-01 15:13:45 +0700

    add new model for learning path

---
+++ lib/models/learning_path.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_dicoding_learning/utils/config/config.dart';
+
+class LearningPath {
+  PathParent path;
+  String desc;
+  String banner;
+
+  LearningPath({
+    required this.path,
+    required this.desc,
+    required this.banner,
+  });
+}
