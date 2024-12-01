d329cc831f2a1f76f378f5cac2f218789c586dc1
Muham <muhambriana.31@gmail.com>
2024-12-01 15:14:18 +0700

    adding new path parent

--- lib/utils/config/path_parent.dart
+++ lib/utils/config/path_parent.dart
@@ -2,6 +2,9 @@ enum PathParent {
   android('Android'),
   ios('IOS'),
   multiPlatform('Multi-Platform'),
+  machineLearning('Machine Learning'),
+  backEndJavaScript('Back-End Javascript'),
+  devOps('DevOps'),
   web('Web');
 
   final String name;
