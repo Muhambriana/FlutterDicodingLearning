61e88cd67aeb82797c7fecb996938ab0835ac850
Muham <muhambriana.31@gmail.com>
2024-12-02 21:42:39 +0700

    remove unused widget

--- lib/ui/course/list/course_list_screen.dart
+++ lib/ui/course/list/course_list_screen.dart
@@ -82,14 +82,14 @@ class CourseList extends StatelessWidget {
       itemCount: courseList.length,
       itemBuilder: (context, index) {
 
-        EdgeInsetsGeometry padding =
+        EdgeInsetsGeometry margin =
         index == 0 ? EdgeInsets.zero:
         index == (courseList.length - 1) ? const EdgeInsets.symmetric(vertical: 10) :
         const EdgeInsets.only(top: 10);
 
         return CourseCard(
           course: courseList[index],
-          padding: padding,
+          margin: margin,
         );
       },
     );
@@ -98,9 +98,9 @@ class CourseList extends StatelessWidget {
 
 class CourseCard extends StatelessWidget {
   final CourseModel course;
-  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
 
-  const CourseCard({required this.course, required this.padding, super.key});
+  const CourseCard({required this.course, required this.margin, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -111,81 +111,78 @@ class CourseCard extends StatelessWidget {
             MaterialPageRoute(
                 builder: (context) => DetailScreen(course: course)));
       },
-      child: Padding(
-        padding: padding,
-        child: Card(
-          margin: EdgeInsets.zero, // Remove default margin of the Card
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                    flex: 1,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(course.banner))),
-                Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          course.name,
-                          style: const TextStyle(fontSize: 16.0),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.star_rate_outlined,
-                              color: Colors.amber,
-                            ),
-                            const SizedBox(width: 8.0),
-                            Text(
-                              course.rating.toString(),
-                              style: Utils.getInformationTextStyle(),
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              flex: 3,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10.0),
-                                child: LinearProgressIndicator(
-                                  value: course.progress / 100,
-                                  // Progress value /100
-                                  color: AppColors.primaryColor,
-                                  backgroundColor: Colors.grey.shade600,
-                                  minHeight: 10,
-                                ),
+      child: Card(
+        margin: margin, // Remove default margin of the Card
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(course.banner))),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        course.name,
+                        style: const TextStyle(fontSize: 16.0),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star_rate_outlined,
+                            color: Colors.amber,
+                          ),
+                          const SizedBox(width: 8.0),
+                          Text(
+                            course.rating.toString(),
+                            style: Utils.getInformationTextStyle(),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: LinearProgressIndicator(
+                                value: course.progress / 100,
+                                // Progress value /100
+                                color: AppColors.primaryColor,
+                                backgroundColor: Colors.grey.shade600,
+                                minHeight: 10,
                               ),
                             ),
-                            const SizedBox(width: 8.0),
-                            Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "${course.progress.toInt()}%",
-                                  style: Utils.getInformationTextStyle(),
-                                )),
-                          ],
-                        ),
-                      ],
-                    ),
+                          ),
+                          const SizedBox(width: 8.0),
+                          Expanded(
+                              flex: 1,
+                              child: Text(
+                                "${course.progress.toInt()}%",
+                                style: Utils.getInformationTextStyle(),
+                              )),
+                        ],
+                      ),
+                    ],
                   ),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
         ),
       ),
