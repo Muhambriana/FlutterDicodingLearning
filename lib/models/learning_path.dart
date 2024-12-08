93f5f139a959334e83d98e321727db0454453e26
Muham <muhambriana.31@gmail.com>
2024-12-08 16:02:36 +0700

    add new field on learning_path model

--- lib/models/learning_path.dart
+++ lib/models/learning_path.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_dicoding_learning/utils/config/config.dart';
 import 'course.dart';
 
@@ -5,12 +7,14 @@ class LearningPath {
   PathParent path;
   String desc;
   String banner;
+  List<Color> colorList;
   List<CourseModel> courseList;
 
   LearningPath({
     required this.path,
     required this.desc,
     required this.banner,
+    required this.colorList,
     required this.courseList,
   });
 }
